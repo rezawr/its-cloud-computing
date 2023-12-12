f57f3a2c1afc8c8a2049a17d1835a4472d3f95c2
rezawr <reza@sahamrakyat.com>
2023-12-12 16:31:58 +0700

    add svm

---
+++ 06.svmtrain.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split, cross_val_predict, KFold
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+balanced_df = pd.read_csv('datasets/balanced.csv')
+
+train, test = train_test_split(balanced_df, test_size=0.3, random_state=12)
+
+min_max_scaler = MinMaxScaler().fit(train[['Flow Duration', 'Tot Fwd Pkts',
+       'Tot Bwd Pkts', 'TotLen Fwd Pkts', 'TotLen Bwd Pkts', 'Fwd Pkt Len Max',
+       'Fwd Pkt Len Min', 'Fwd Pkt Len Mean', 'Fwd Pkt Len Std',
+       'Bwd Pkt Len Max', 'Bwd Pkt Len Min', 'Bwd Pkt Len Mean',
+       'Bwd Pkt Len Std', 'Flow Byts/s', 'Flow Pkts/s', 'Flow IAT Mean',
+       'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Tot',
+       'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min',
+       'Bwd IAT Tot', 'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max',
+       'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags',
+       'Bwd URG Flags', 'Fwd Header Len', 'Bwd Header Len', 'Fwd Pkts/s',
+       'Bwd Pkts/s', 'Pkt Len Min', 'Pkt Len Max', 'Pkt Len Mean',
+       'Pkt Len Std', 'Pkt Len Var', 'FIN Flag Cnt', 'SYN Flag Cnt',
+       'RST Flag Cnt', 'PSH Flag Cnt', 'ACK Flag Cnt', 'URG Flag Cnt',
+       'CWE Flag Count', 'ECE Flag Cnt', 'Down/Up Ratio', 'Pkt Size Avg',
+       'Fwd Seg Size Avg', 'Bwd Seg Size Avg', 'Fwd Byts/b Avg',
+       'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg', 'Bwd Byts/b Avg',
+       'Bwd Pkts/b Avg', 'Bwd Blk Rate Avg', 'Subflow Fwd Pkts',
+       'Subflow Fwd Byts', 'Subflow Bwd Pkts', 'Subflow Bwd Byts',
+       'Init Fwd Win Byts', 'Init Bwd Win Byts', 'Fwd Act Data Pkts',
+       'Fwd Seg Size Min', 'Active Mean', 'Active Std', 'Active Max',
+       'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min']])
+
+
+numerical_columns = ['Flow Duration', 'Tot Fwd Pkts',
+       'Tot Bwd Pkts', 'TotLen Fwd Pkts', 'TotLen Bwd Pkts', 'Fwd Pkt Len Max',
+       'Fwd Pkt Len Min', 'Fwd Pkt Len Mean', 'Fwd Pkt Len Std',
+       'Bwd Pkt Len Max', 'Bwd Pkt Len Min', 'Bwd Pkt Len Mean',
+       'Bwd Pkt Len Std', 'Flow Byts/s', 'Flow Pkts/s', 'Flow IAT Mean',
+       'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Tot',
+       'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min',
+       'Bwd IAT Tot', 'Bwd IAT Mean', 'Bwd IAT Std', 'Bwd IAT Max',
+       'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags',
+       'Bwd URG Flags', 'Fwd Header Len', 'Bwd Header Len', 'Fwd Pkts/s',
+       'Bwd Pkts/s', 'Pkt Len Min', 'Pkt Len Max', 'Pkt Len Mean',
+       'Pkt Len Std', 'Pkt Len Var', 'FIN Flag Cnt', 'SYN Flag Cnt',
+       'RST Flag Cnt', 'PSH Flag Cnt', 'ACK Flag Cnt', 'URG Flag Cnt',
+       'CWE Flag Count', 'ECE Flag Cnt', 'Down/Up Ratio', 'Pkt Size Avg',
+       'Fwd Seg Size Avg', 'Bwd Seg Size Avg', 'Fwd Byts/b Avg',
+       'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg', 'Bwd Byts/b Avg',
+       'Bwd Pkts/b Avg', 'Bwd Blk Rate Avg', 'Subflow Fwd Pkts',
+       'Subflow Fwd Byts', 'Subflow Bwd Pkts', 'Subflow Bwd Byts',
+       'Init Fwd Win Byts', 'Init Bwd Win Byts', 'Fwd Act Data Pkts',
+       'Fwd Seg Size Min', 'Active Mean', 'Active Std', 'Active Max',
+       'Active Min', 'Idle Mean', 'Idle Std', 'Idle Max', 'Idle Min']
+
+train.drop(['Timestamp'], axis=1,inplace=True)
+test.drop(['Timestamp'],axis=1,inplace=True)
+
+train[numerical_columns] = min_max_scaler.transform(train[numerical_columns])
+
+
+grid_search_results = pd.read_csv('datasets/gridcvsvm.csv')
+
+best_params = grid_search_results.loc[grid_search_results['mean_test_score'].idxmax(), ['param_c', 'param_gamma']]
+
+y_train = np.array(train.pop("Label"))
+X_train = train.values
+
+X_train
+
+y_test = np.array(test.pop("Label"))
+X_test = test.values
+
+X_test
+svc = SVC()
+svc.set_params(C=best_params['param_c'], gamma=best_params['param_gamma'])
+
+svc.fit(X_train, y_train)
+
+
+y_pred = svc.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred, average='weighted')
+precision = precision_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
+
+print(f"Accuracy: {accuracy}")
+
+single_result = pd.DataFrame([{
+    'Accuracy': accuracy,
+    'Recall': recall,
+    'Precision': precision,
+    'F1-Score': f1,
+}])
+
+
+
+
+
+# Lists to store results for each fold
+kf = KFold(n_splits=10, shuffle=True, random_state=42)
+fold_results = []
+
+for fold, (train_idx, val_idx) in enumerate(kf.split(X_train)):
+    X_fold_train, X_fold_val = X_train[train_idx], X_train[val_idx]
+    y_fold_train, y_fold_val = y_train[train_idx], y_train[val_idx]
+
+    svc.fit(X_fold_train, y_fold_train)
+    y_pred = svc.predict(X_fold_val)
+
+    # Calculate metrics for each fold
+    accuracy = accuracy_score(y_fold_val, y_pred)
+    recall = recall_score(y_fold_val, y_pred, average='weighted')
+    precision = precision_score(y_fold_val, y_pred, average='weighted')
+    f1 = f1_score(y_fold_val, y_pred, average='weighted')
+
+    # Save metrics into a DataFrame
+    fold_results.append({
+        'Fold': fold + 1,
+        'Accuracy': accuracy,
+        'Recall': recall,
+        'Precision': precision,
+        'F1-Score': f1,
+    })
+# Save overall metrics to a CSV file
+results_df = pd.DataFrame(fold_results)
+results_df.to_csv('result/cv_svc.csv', index=False)
+single_result.to_csv('result/single_svc.csv', index=False)
